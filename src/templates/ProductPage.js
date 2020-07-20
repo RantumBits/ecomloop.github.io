5df2adc4476b54b547b170ac3f528bf1cdb913a2
ecomloop <andrew@ecomloop.com>
2020-07-19 20:22:25 -0700

    update product pages to show blog posts

--- src/templates/ProductPage.js
+++ src/templates/ProductPage.js
@@ -2,16 +2,40 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Image from 'gatsby-image'
 import { ChevronLeft } from 'react-feather'
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import PageHeader from '../components/PageHeader'
 import Layout from '../components/Layout'
 import ProductForm from '../components/ProductForm'
+import PostCard from '../components/PostCard'
 import ProductGalleryThumbnails from '../components/ProductGalleryThumbnails'
 
 import './ProductPage.css'
 
 const ProductPage = ({ data }) => {
     const product = data.shopifyProduct
+    console.log(product)
+    
+    //find posts corresponding to current product
+    const productBlogTagMap = {
+      "ecommerce-platform-strategy-plan" : "digital commerce",
+      "ecommcerce-conversion-rate-optimization-services" : "conversion rate optimization",
+      "shopify-help" : "shopify",
+      "gatsbyjs" : "gatsbyjs",
+      "paid-search-help" : "search"
+    };
+    const searchTag = productBlogTagMap[product.handle];    
+    const postEdges = data.allPosts.edges;
+    const filterdPostEdges = [];
+    postEdges.map((edge)=>{
+      if(edge.node.frontmatter.tags && edge.node.frontmatter.tags.indexOf(searchTag)>-1) {
+        filterdPostEdges.push(edge.node)
+      }
+    });
+    console.log("******* searchTag = "+searchTag)
+    console.log("******* product.handle = "+product.handle)
+    console.log(filterdPostEdges)    
+    
     const thisEdge = data.allServices.edges.find(edge => edge.node.id === product.id);
 
     return (
@@ -37,7 +61,30 @@ const ProductPage = ({ data }) => {
 
                         <div className="SingleService--InnerContent">
                             <ProductForm product={product} />
-                            <div dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
+                            <Tabs>
+                              <TabList>
+                                <Tab>Description</Tab>
+                                <Tab>Blog Posts</Tab>
+                              </TabList>
+                              <TabPanel>
+                                <div dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
+                              </TabPanel>
+                              <TabPanel>
+                                {!!filterdPostEdges.length && (
+                                  <div className="PostSection--Grid" style={{gridGap: "1rem"}}>
+                                    {filterdPostEdges.map((post, index) => (
+                                      <PostCard key={index} 
+                                        featuredImage={`../${post.frontmatter.featuredImage}`}
+                                        title={post.frontmatter.title}
+                                        excerpt={post.excerpt}
+                                        date={post.frontmatter.date}
+                                        slug={post.fields.slug}
+                                      />
+                                    ))}
+                                  </div>
+                                )}
+                              </TabPanel>
+                            </Tabs>                            
                         </div>
 
                         <div className="SingleService--Pagination">
@@ -112,6 +159,29 @@ export const pageQuery = graphql`
             fluid(maxWidth: 910) {
               ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
+          }
+        }
+      }
+    }
+    
+    allPosts: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "posts" } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "dddd MMMM DD, YYYY")
+            categories {
+              category
+            }
+            featuredImage
+            tags
           }
         }
       }
