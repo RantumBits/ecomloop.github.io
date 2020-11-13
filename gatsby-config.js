766e42ee9d45773aa98b6fbf983cbc14b6cefd85
andrew @ ecomloop <andrew@ecomloop.com>
2020-11-12 18:10:56 -0800

    rollback

--- gatsby-config.js
+++ gatsby-config.js
@@ -137,6 +137,28 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-offline',
+      options: {
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`
+          },
+          {
+            // uploadcare
+            urlPattern: /^https:\/\/ucarecdn.com\/[-a-zA-Z0-9@:%_\+.~#?&//=]*?\/10x\//,
+            handler: `staleWhileRevalidate`
+          }
+        ],
+        skipWaiting: true,
+        clientsClaim: true
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -205,7 +227,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-      
+        postCssPlugins: [
+          postcssPresetEnv({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ]
       }
     },
     {
